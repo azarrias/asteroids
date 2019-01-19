a360758e6d17fb3b35f713e93329cb61b1b25ad8
azarrias <adolfo.zarrias@gmail.com>
2019-01-19 18:04:42 +0100

    Handle onclick events through code

--- app/src/main/java/com/wax_tadpole_games/android/asteroids/MainActivity.java
+++ app/src/main/java/com/wax_tadpole_games/android/asteroids/MainActivity.java
@@ -4,13 +4,23 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button btnAbout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btnAbout = findViewById(R.id.btn_about);
+        btnAbout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                launchAbout(null);
+            }
+        });
     }
 
     public void launchAbout(View view) {
