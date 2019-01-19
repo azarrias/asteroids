7805f15ea3e1202cb8e40099bf54866561f4cfe0
azarrias <adolfo.zarrias@gmail.com>
2019-01-19 23:36:16 +0100

    Add activity menu

--- app/src/main/java/com/wax_tadpole_games/android/asteroids/MainActivity.java
+++ app/src/main/java/com/wax_tadpole_games/android/asteroids/MainActivity.java
@@ -3,6 +3,8 @@ package com.wax_tadpole_games.android.asteroids;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
@@ -36,5 +38,24 @@ public class MainActivity extends AppCompatActivity {
     public void launchAbout(View view) {
         Intent i = new Intent(this, AboutActivity.class);
         startActivity(i);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        if (id == R.id.about) {
+            launchAbout(null);
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
