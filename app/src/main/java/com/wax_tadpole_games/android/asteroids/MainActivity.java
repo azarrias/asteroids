360f7cc9fc9e203c1d5fc9d9f2b4294e44353d47
azarrias <adolfo.zarrias@gmail.com>
2019-02-08 21:56:39 +0100

    Temporarily show preferences when pressing Play button

--- app/src/main/java/com/wax_tadpole_games/android/asteroids/MainActivity.java
+++ app/src/main/java/com/wax_tadpole_games/android/asteroids/MainActivity.java
@@ -1,15 +1,19 @@
 package com.wax_tadpole_games.android.asteroids;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.preference.PreferenceManager;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button btnPlay;
     private Button btnAbout;
     private Button btnExit;
     private Button btnSetup;
@@ -19,6 +23,14 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        btnPlay = findViewById(R.id.btn_start);
+        btnPlay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showPreferences(v);
+            }
+        });
+        
         btnAbout = findViewById(R.id.btn_about);
         btnAbout.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -42,6 +54,17 @@ public class MainActivity extends AppCompatActivity {
                 finish();
             }
         });
+    }
+
+    private void showPreferences(View view) {
+        SharedPreferences pref = PreferenceManager.getDefaultSharedPreferences(this);
+        String str = "music: " + pref.getBoolean("music", true)
+                + ", graphics: " + pref.getString("graphics", "?")
+                + ", fragments: " + pref.getString("fragments", "?")
+                + ", multiplayer: " + pref.getBoolean("multiplayer", true)
+                + ", max_players: " + pref.getString("max_players", "?")
+                + ", connection_type: " + pref.getString("connection_type", "?");
+        Toast.makeText(this, str, Toast.LENGTH_SHORT).show();
     }
 
     public void launchAbout(View view) {
