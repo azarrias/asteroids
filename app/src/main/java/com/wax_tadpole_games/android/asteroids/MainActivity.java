e5004270790c5c09995d856160e2527cdab06705
azarrias <adolfo.zarrias@gmail.com>
2019-02-09 01:43:03 +0100

    Add listener for asteroid number of pieces preference change

--- app/src/main/java/com/wax_tadpole_games/android/asteroids/MainActivity.java
+++ app/src/main/java/com/wax_tadpole_games/android/asteroids/MainActivity.java
@@ -2,8 +2,8 @@ package com.wax_tadpole_games.android.asteroids;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.preference.PreferenceManager;
 import android.view.Menu;
 import android.view.MenuItem;
--- app/src/main/java/com/wax_tadpole_games/android/asteroids/PreferencesFragment.java
+++ app/src/main/java/com/wax_tadpole_games/android/asteroids/PreferencesFragment.java
@@ -3,11 +3,10 @@ package com.wax_tadpole_games.android.asteroids;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.preference.EditTextPreference;
+import android.support.v7.preference.Preference;
 import android.support.v7.preference.PreferenceFragmentCompat;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.TextView;
+import android.widget.Toast;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -17,6 +16,27 @@ public class PreferencesFragment extends PreferenceFragmentCompat {
     @Override
     public void onCreatePreferences(Bundle bundle, String s) {
         addPreferencesFromResource(R.xml.preferences);
+
+        final EditTextPreference fragments = (EditTextPreference)findPreference("fragments");
+        fragments.setOnPreferenceChangeListener(new Preference.OnPreferenceChangeListener() {
+            @Override
+            public boolean onPreferenceChange(Preference preference, Object newValue) {
+                int value;
+                try {
+                    value = Integer.parseInt((String)newValue);
+                } catch(Exception e) {
+                    Toast.makeText(getActivity(), "Error: must enter a numeric value", Toast.LENGTH_SHORT).show();
+                    return false;
+                }
+                if (value >= 0 && value <= 9) {
+                    fragments.setSummary("Number of pieces an asteroid is divided into (" + value + ")");
+                    return true;
+                } else {
+                    Toast.makeText(getActivity(), "Number of pieces must be between 0 and 9", Toast.LENGTH_SHORT).show();
+                    return false;
+                }
+            }
+        });
     }
 
 }
